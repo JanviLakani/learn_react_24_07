49121273c261a2660aa0ae17c458b5d7b37207a6
JanviLakani <lakanijanvi1999@gmail.com>
2025-07-24 23:53:08 +0530

    add_props_state_24_07_h_w

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Greeting from './Greeting';
 function App() {
   const [count, setCount] = useState(0)
-
+ 
   return (
     <>
       <div>
@@ -32,6 +32,10 @@ function App() {
       <h2>testing</h2>
 
       <h3>testing 1</h3>
+
+      <Greeting name="Janvi"  />
+
+      <Greeting name="Priyanka" />
       
     </>
   )
---
+++ src/Greeting.jsx
@@ -0,0 +1,9 @@
+
+function Greeting(props) {
+  return  (
+
+    <h2>Hello, Good Morning {props.name} </h2>
+  );
+}
+
+export default Greeting;
